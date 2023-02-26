ac9b0a3927ee97f96c11c36d3744b57b934415cf
Tyler Chen <tyche4490@gmail.com>
2023-02-25 19:25:10 -0800

    working fully addressable 8x8

--- tilted.ino
+++ tilted.ino
@@ -1,25 +1,38 @@
-int mask[4][8] = {
+int mask[8][8] = {
   {0, 1, 1, 1, 0, 0, 0, 0},
   {1, 1, 0, 0, 0, 0, 0, 0},
   {1, 1, 1, 1, 0, 0, 0, 0},
+  {0, 1, 0, 1, 0, 0, 0, 0},
+  {0, 1, 0, 1, 0, 0, 0, 0},
+  {0, 1, 0, 1, 0, 0, 0, 0},
+  {0, 1, 0, 1, 0, 0, 0, 0},
   {0, 1, 0, 1, 0, 0, 0, 0}
 };
 
+const int clockPin=12, dataPin=11, latchPin=10;
+
+void sendBits(int data) {
+  // helper function that makes it simple to set the contents of the shift register
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, data);
+  digitalWrite(latchPin, HIGH);
+}
 void setup() {
-  // put your setup code here, to run once:
-  for (int i=2; i<=13; ++i)
+  //pins 2-9 row headers, pins 10-12 for shift register
+  for (int i=2; i<=12; ++i)
     pinMode(i, OUTPUT);
 
 }
 
 void loop() {
-  for (int r=0; r<4; ++r) {
-    for (int c=0; c<8; ++c) {
-      digitalWrite(c+6, (mask[r][c])?LOW:HIGH);
-    }
+  for (int r=0; r<8; ++r) {
+    int reg=0;
+    for (int c=0; c<8; ++c) reg |= (mask[r][c]<<c);
+    sendBits(reg);
     digitalWrite(r+2, HIGH);
     delay(1);
     digitalWrite(r+2, LOW);
+
   }
 
   // put your main code here, to run repeatedly:
